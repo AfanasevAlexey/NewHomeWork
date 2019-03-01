f96a3a825e165b33121e8773d46861326ce8bd47
AfanasevAlexey <ya.alexey.27@mail.ru>
2019-03-01 22:21:29 +0900

    я приветствую мир

---
+++ New Home Work/New Home Work/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  New Home Work
+//
+//  Created by Алексей on 01/03/2019.
+//  Copyright © 2019 Alexey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
